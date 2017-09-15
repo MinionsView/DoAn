91dcad6f89eb56212d95dd1c1a16d5cad94f4398
doangiavy <giavyroyal@gmail.com>
2017-09-15 15:08:56 +0700

    Update package  Server

---
+++ DoAn/src/Server/Server.java
@@ -0,0 +1,15 @@
+package Server;
+
+public class Server {
+	
+	public Server(int screen, int mouseclick, int mousemove) {
+		ScreenThread st = new ScreenThread(screen);
+		new Thread(st).start();
+		MouseThread mt = new MouseThread(mousemove);
+		new Thread(mt).start();
+		MouseClickThread mct = new MouseClickThread(mouseclick);
+		new Thread(mct).start();
+		
+	}
+
+}
